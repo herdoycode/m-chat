cd39af290a14f4a7dc4fd050b01e1d73608b9969
= <=>
2023-08-26 20:03:51 +0600

    Update db url

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://chat-api-node.onrender.com/api",
   headers: {
     "x-auth-token": token ? token : "",
   },
