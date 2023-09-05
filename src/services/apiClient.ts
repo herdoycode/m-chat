bdbbfd565d3706ea2e5eb1841fb37c88ef3367f4
= <=>
2023-09-05 18:44:57 +0600

    Add server api

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 const token = localStorage.getItem("token");
 
-// const url = "https://chat-api-node.onrender.com/api";
+const url = "https://chat-api-node.onrender.com/api";
 
 export default axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: url,
   headers: {
     "x-auth-token": token ? token : "",
   },
